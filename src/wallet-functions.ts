6f05e009ce147cff37284118181db90344477808
Ulrik de Muelenaere <ulrikdem@gmail.com>
2018-08-29 15:28:46 +0200

    Implement wallet functions

--- src/wallet-functions.ts
+++ src/wallet-functions.ts
@@ -1,7 +1,14 @@
-export function hasWallet(){
-  return false;
-};
+import Web3 from 'web3';
 
-export function getAddress(){
-  return "";
+let web3;
+if('web3' in window) {
+  web3 = new Web3(window['web3'].currentProvider);
+}
+
+export function hasWallet() {
+  return web3 !== undefined && web3.eth.defaultAccount !== undefined;
+}
+
+export function getAddress() {
+  return web3.eth.defaultAccount;
 }
