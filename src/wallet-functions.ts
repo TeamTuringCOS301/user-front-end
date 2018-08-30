c8cc234bdf2bf6c1605184b9c1c2865c3a6b905b
Richard Dixie <u16004303@tuks.co.za>
2018-08-30 12:15:44 +0200

    Reverted change in wallet function

--- src/wallet-functions.ts
+++ src/wallet-functions.ts
@@ -2,7 +2,7 @@ import Web3 from 'web3';
 
 let web3;
 if('web3' in window) {
-  web3 = new Web3(window["web3"].currentProvider);
+  web3 = new Web3(window['web3'].currentProvider);
 }
 
 export function hasWallet() {
