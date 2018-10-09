4492e2e04dfd961334bf6b154f190ac22461bf6c
Richard Dixie <u16004303@tuks.co.za>
2018-10-09 17:20:33 +0200

    Fixed url for hosting

--- src/app-config.ts
+++ src/app-config.ts
@@ -1,5 +1,5 @@
 export let CONFIG ={
-  url: "https://erpcoin.westeurope.cloudapp.azure.com/api",
+  url: "/api",
   interval: 10000, //in milliseconds
   severity: ["Animal Sighting", "Warning/Caution", "Critical Alert"],
   alertSideLengthPx: 512,
